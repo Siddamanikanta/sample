9b21e83a6920beef123d1a73a7890ea8a71e7e18
venkat <siddamanikanta869@gmail.com>
2025-11-29 19:38:28 +0530

    updated df

--- sam.py
+++ sam.py
@@ -16,9 +16,3 @@ hr_df = pd.read_csv(path + "/heart_failure_clinical_records.csv")
 # Print the dataframe
 print(hr_df)
 
-# Plot the target feature count
-sns.countplot(x=hr_df['DEATH_EVENT'], palette="Set2")
-plt.title("Target Distribution (DEATH_EVENT)")
-plt.xlabel("Class (0 = Alive, 1 = Death)")
-plt.ylabel("Count")
-plt.show()
